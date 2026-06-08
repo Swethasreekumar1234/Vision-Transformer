822e3c9cb067e51281aba4f7d853f20b6a507bb6
Swetha <148451524+Swethasreekumar1234@users.noreply.github.com>
2026-06-08 15:51:53 +0530

    Increase training epochs and adjust parameters

--- test_yaml.py
+++ test_yaml.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8s.pt')
-model.train(data='data.yaml', epochs=1, imgsz=640, batch=8)
+model.train(data='data.yaml', epochs=30, imgsz=1024, batch=4,patience=10,optimizer='AdamW')
